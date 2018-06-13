8ddde7bbf4de0b76d8d619d0c63b042bfd23e94e
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-13 15:33:29 +0200

    Update selfil.sh

--- selfil.sh
+++ selfil.sh
@@ -2,37 +2,43 @@
 #
 # faire un choix parmi les fichiers d'un répertoire fourni en entrée
 
-# parser l'entrée
-[ -n "$1" ] && F="$1" || F="$PWD"
+function slct () {
 
-#tester l'existence du repertoire
-[ ! -d "$F" ] && exit 1
+	# parser l'entrée
+	[ -n "$1" ] && F="$1" || F="$PWD"
 
-#sortie temporaire
-tf=$(mktemp)
+	#tester l'existence du repertoire
+	[ ! -d "$F" ] && exit 1
 
-find "$F" -maxdepth 1 -type f | busybox awk -v answer="$tf" '
-function read_console() {
-	"head -1 < /dev/tty" | getline results
-	close("head -1 < /dev/tty")
-	return results
+	#sortie temporaire
+	tf=$(mktemp)
+
+	#lister les fichiers, présenter, demander le choix
+	find "$F" -maxdepth 1 -type f | busybox awk -v answer="$tf" '
+	function read_console() {
+		"head -1 < /dev/tty" | getline results
+		close("head -1 < /dev/tty")
+		return results
+	}
+
+	{ A[NR] = $0 }
+
+	END {
+		if ( NR < 1 ) break
+		do {
+			for (l=1; l<=NR; l++) { printf "\t%2d. %s\n", l, A[l] }
+			printf "\nEnter selection ( 1 - %s ) or (q)uit: ", NR
+			selection = read_console()
+			if (selection == "q") break
+			selection = selection + 0
+		} while (selection < 1 || selection > NR)
+		if (selection != "q") print A[selection] > answer
+	} '
+
+	#afficher le choix
+	cat "$tf"
+
+	# nettoyer
+	rm -f "$tf"
+
 }
-
-{ A[NR] = $0 }
-
-END {
-	if ( NR < 1 ) break
-	do {
-		for (l=1; l<=NR; l++) { printf "\t%2d. %s\n", l, A[l] }
-		printf "\nEnter selection ( 1 - %s ) or (q)uit: ", NR
-		selection = read_console()
-		if (selection == "q") break
-		selection = selection + 0
-	} while (selection < 1 || selection > NR)
-	if (selection != "q") print A[selection] > answer
-} ' < "$FILEIN"
-
-
-chx=$(cat "$tf")
-rm -f "$tf"
-printf "%s" "$chx"
