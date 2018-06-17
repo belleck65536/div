58023b7c61fec671983453cf70b85fe8bc990826
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-17 21:17:30 +0000

    Update selfil.sh

--- selfil.sh
+++ selfil.sh
@@ -1,44 +1,36 @@
 #!/bin/busybox ash
-#
-# faire un choix parmi les fichiers d'un répertoire fourni en entrée
 
 function slct () {
 
-	# parser l'entrée
-	[ -n "$1" ] && F="$1" || F="$PWD"
+ # sortie temporaire
+ tf=$(mktemp)
 
-	#tester l'existence du repertoire
-	[ ! -d "$F" ] && exit 1
+ # présenter, demander le choix
+ echo "$@" | busybox awk -v answer="$tf" '
+ function read_console() {
+  "head -1 < /dev/tty" | getline results
+  close("head -1 < /dev/tty")
+  return results
+ }
 
-	#sortie temporaire
-	tf=$(mktemp)
+ { A[NR] = $0 }
 
-	#lister les fichiers, présenter, demander le choix
-	find "$F" -maxdepth 1 -type f | busybox awk -v answer="$tf" '
-	function read_console() {
-		"head -1 < /dev/tty" | getline results
-		close("head -1 < /dev/tty")
-		return results
-	}
+ END {
+  if ( NR < 1 ) break
+  for (l=1; l<=NR; l++) { printf "\t%2d. %s\n", l, A[l] }
+  do {
+   printf "Enter selection ( 1 - %s ) or (q)uit: ", NR
+   selection = read_console()
+   if (selection == "q") break
+   selection = selection + 0
+  } while (selection < 1 || selection > NR)
+  if (selection != "q") print A[selection] > answer
+ } '
 
-	{ A[NR] = $0 }
+ # afficher le choix
+ cat "$tf"
 
-	END {
-		if ( NR < 1 ) break
-		do {
-			for (l=1; l<=NR; l++) { printf "\t%2d. %s\n", l, A[l] }
-			printf "\nEnter selection ( 1 - %s ) or (q)uit: ", NR
-			selection = read_console()
-			if (selection == "q") break
-			selection = selection + 0
-		} while (selection < 1 || selection > NR)
-		if (selection != "q") print A[selection] > answer
-	} '
-
-	# afficher le choix
-	cat "$tf"
-
-	# nettoyer
-	rm -f "$tf"
+ # nettoyer
+ rm -f "$tf"
 
 }
