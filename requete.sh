930ff85724341ca646eedc3cedff93e78f412c00
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-19 15:32:11 +0200

    Update requete.sh

--- requete.sh
+++ requete.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-# permet la creation d'une requete de certificat puis signe le certificat
 #
-# WORKDIR NOMFIC REQCFG MODREQ
 # "${1:-/dev/stdin}" --> prend $1 si stdin est vide
 
 . ./select.sh
 
 WORKDIR=certs/
-ec="Courbe eliptique"
-rsa="Paire RSA"
+EC="Courbe eliptique"
+RSA="Paire RSA"
 
 while [ "$NOMFIC" = "" ]; do
 	read -p "Nom de fichier pour la requête (l'extension sera ajoutée automatiquement) : " NOMFIC
@@ -19,31 +17,31 @@ while [ "$NOMFIC" = "" ]; do
 done
 
 echo "Type de clef ?"
-clef=$( slct "$ec" "$rsa" )
+CLEF=$( slct "$EC" "$RSA" )
 
-case "$clef" in 
-	"$ec")
+case "$CLEF" in 
+	"$EC")
 		echo "Type de courbe :"
-		keyargs=$( slct secp521r1 secp384r1 prime256v1 )
-		[ -n "$keyargs" ] && keyargs="ecparam -genkey -noout -name $keyargs" || exit
+		KEYARGS=$( slct secp521r1 secp384r1 prime256v1 )
+		[ -n "$KEYARGS" ] && KEYARGS="ecparam -genkey -noout -name $KEYARGS" || exit
 	;;
-	"$rsa")
+	"$RSA")
 		echo "Longueur de clef :"
-		keyargs=$( slct 1024 2048 4096 8192 )
-		[ -n "$keyargs" ] && keyargs="genrsa $keyargs" || exit
+		KEYARGS=$( slct 1024 2048 4096 8192 )
+		[ -n "$KEYARGS" ] && KEYARGS="genrsa $KEYARGS" || exit
 	;;
 	*) exit ;;
 esac
 
 echo "Modèle de requête :"
-cfg_file=$( slct $( ls ./etc/req*.conf ) )
+CFG_FILE=$( slct $( ls ./etc/req*.conf ) )
 
 # recherche des extensions disponibles
-ext="$( slct $( grep -e "\[.*_ext\s*\]" "$cfg_file" | sed -r 's/\[\s*//g' | sed -r 's/_ext\s*\]//g' | tr '\n' ' ') )_ext"
-[ -z "$ext" ] && exit
+EXT="$( slct $( grep -e "\[.*_ext\s*\]" "$CFG_FILE" | sed -r 's/\[\s*//g' | sed -r 's/_ext\s*\]//g' | tr '\n' ' ') )_ext"
+[ -z "$EXT" ] && exit
 
 # ajout d'subjectAltName suivant l'extension demandée
-if [ $(echo "$ext" | grep -ic "san" ) -ge 1 ] ; then
+if [ $(echo "$EXT" | grep -ic "san" ) -ge 1 ] ; then
 	printf "%s\n" "Définition du SAN suivant la forme <type>:<valeur>,<type>:<valeur>,..."
 	printf "%s\n" "éléments :\n\tDNS\n\tIP\n\tURI\n\temail\n\tRID\n\tdirName\n\totherName"
 	printf "%s\n" "ex : DNS:www.example.com,IP:0.0.0.0"
@@ -51,13 +49,13 @@ if [ $(echo "$ext" | grep -ic "san" ) -ge 1 ] ; then
 		read SANC
 		case $SANC in
 			"") echo "???" ;;
-			*)	read -p "Appuyez sur une touche pour continuer" d
+			*)	read -p "Appuyez sur une touche pour continuer" D
 				SANC="SAN=$SANC"
 				;;
 		esac
 	done
 fi
 	
-openssl $keyargs -out "$WORKDIR$NOMFIC.key"
-$SANC openssl req -new -config "etc/$cfg_file" -reqexts $ext -out "$WORKDIR$NOMFIC.csr" -key "$WORKDIR$NOMFIC.key"
-# ./signat.sh $REQCFG "$WORKDIR$NOMFIC.csr"
+openssl $KEYARGS -out "$WORKDIR$NOMFIC.key"
+$SANC openssl req -new -config "$CFG_FILE" -reqexts $EXT -out "$WORKDIR$NOMFIC.csr" -key "$WORKDIR$NOMFIC.key"
+# ./signat.sh $CFG_FILE "$WORKDIR$NOMFIC.csr"
