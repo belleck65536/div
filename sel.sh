0f8dc36e60d6871168397e24dc80eb06bd097537
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-19 12:20:15 +0200

    Update sel.sh

--- sel.sh
+++ sel.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 function slct () {
-	local i r c
+	local m i r c
 
 	for A in "$@" ; do
 		# contruction de l'index i
 		let i+=1
 
 		# obtention de l'index max
-		map="$i"
+		m="$i"
 
 		# mapping
-		eval map$i=\"$A\"
+		eval local m$i=\"$A\"
 		
 		# affichage
 		printf "\t%2d. %s\n" "$i" "$A" >&2
 	done
 
-	while [ -z $r ] ; do
+	while [ -z "$r" ] ; do
 		# demande du choix
-		read -p "Enter selection ( 1 - $map ) or (q)uit: " c
+		read -p "Enter selection ( 1 - $m ) or (q)uit: " c
 
 		# sortie demandée ?
 		[ "$c" = "q" ] && break
@@ -29,7 +29,7 @@ function slct () {
 		[ $? -ne 0 ] && continue
 		
 		# sommes-nous dans la bonne plage numérique ?
-		[ $c -ge 1 -a $c -le $map ] && r="$(eval echo \$map$c)"
+		[ $c -ge 1 -a $c -le $m ] && eval r="\$m$c"
 	done
 
 	printf "%s" "$r"
