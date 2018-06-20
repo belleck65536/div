2c349428160053eb85f1548de29fdfdda4e213df
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-20 17:07:59 +0200

    Update signat.sh

--- signat.sh
+++ signat.sh
@@ -1,39 +1,53 @@
 #!/bin/sh
-#set CACFG
 #
-# checker si nombre d'arguments = 2
-#	then :checker existence de la conf en arg1 et checker existence de la csr en arg2
+# -c "config_file" -e "extension" -i "req_file"
 
+. ./select.sh
 
 function display_help () {
 	echo "Usage : $(basename $0) <configuration> <extensions> <requete>"
 	exit
 }
 
-case "$#" in
-	3)
-		[ -f "$1" ] && cfg_file="$1" || ( echo "erreur sur fichier de configuration \"$1\"" ; exit )
-		[ -f "$3" ] && req_file="$3" || ( echo "erreur sur fichier de requête \"$3\"" ; exit )
-		[ -f "${req_file%.csr}.crt" ] && ( echo "Nom de certificat signé déjà utilisé \"${req_file%.csr}.crt\"" ; exit )
-		[ $( grep -ec "\s*\[\s*${2}_ext\s*\]\s*" ) -ge 1 ] && exten="${2}_ext" || ( echo "extensions non trouvées \"$2\"" ; exit )
-	;;
-	0)
-		# choisir configuration (une conf portant le même nom que les dossiers [dossier=cert promu CA] d'autorité signataire)
-		# choisir requête (critères : extension .csr, pas de .crt associé)
-		# assigner variable pour signature
-	;;
-	*)
-		display_help
-	;;
-esac
+while getopts ":c:e:i:" opt; do
+	case $opt in
+		c)
+			[ -f "$OPTARG" ] && cfg_file="$OPTARG" || ( echo "erreur sur fichier de configuration \"$OPTARG\"" ; exit 1 )
+		;;
+		e)
+			exten="$OPTARG"
+		;;
+		i)
+			[ -f "$OPTARG" ] && req_file="$OPTARG" || ( echo "erreur sur fichier de requête \"$OPTARG\"" ; exit 1 )
+		;;
+		\?)
+			echo "Invalid option: -$OPTARG" ; display_help
+		;;
+		:)
+			echo "Option -$OPTARG requires an argument." ; display_help
+		;;
+	esac
+done
+
+# validation i
+[ -z "$req_file" ] && # choisir req_file dans une liste excluant les csr ayant un crt, si liste vide, renvoyer un message d'erreur + sortie
+#[ -f "${req_file%.csr}.crt" ] && ## choisir à nouveau					( echo "Nom de certificat signé déjà utilisé \"${req_file%.csr}.crt\"" ; exit 1 )
+
+# validation c
+[ -z "$cfg_file" ] && # choisir config, si liste vide, sortir
+
+# validation e
+[ -z "$exten" -o $( grep -ec "\s*\[\s*$2\s*\]\s*" ) -lt 1 ] && # choisir extensions dans config
+si exten toujours vide, quitter sur anomalie extension dans config_file
+
 
 openssl ca -config "$cfg_file" -in "$req_file" -out "${req_file%.csr}.crt" -extensions "$exten" -notext
 
-if [ -f "${2%.csr}.key" -a -f "${2%.csr}.crt" ]; then
+if [ -f "${req_file%.csr}.key" -a -f "${req_file%.csr}.crt" ]; then
 	read -p "Convertir le certificat signé en PKCS#12 ? [y/n] "
-	[ "${REPLY::1}" = "y" ] && openssl pkcs12 -export -inkey "${2%.csr}.key" -in "${2%.csr}.crt" -out "${2%.csr}.p12"
+	[ "${REPLY::1}" = "y" ] && openssl pkcs12 -export -inkey "${req_file%.csr}.key" -in "${req_file%.csr}.crt" -out "${req_file%.csr}.p12"
 	read -p "Supprimer les fichiers locaux de cette certification ? [y/n] "
-	[ "${REPLY::1}" = "y" ] && rm -f "${2%.csr}."*
+	[ "${REPLY::1}" = "y" ] && rm -f "${req_file%.csr}."*
 fi
 
-cp "${2%.csr}."* /home/cert/
+cp "${req_file%.csr}."* /home/cert/
