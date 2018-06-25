a9ba26e1584f0f2689b8516f08253f1a3131cc33
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-25 16:29:23 +0200

    Update signat.sh

--- signat.sh
+++ signat.sh
@@ -39,14 +39,14 @@ done
 # validation i
 # si pas d'argument fourni, on recherche une CSR
 if [ -z "$req_file" ] ; then
-	slct $( for csr in $CSRDIR/*.csr ; do [ ! -f "${csr%.csr}.crt" ] && echo "$csr" ; done  )
+	slct $( for csr in $(ls -1 $CSRDIR/*.csr) ; do [ ! -f "${csr%.csr}.crt" ] && echo "$csr" ; done  )
 fi
 # si on a toujours pas d'arguments, c'est qu'il n'y avait rien en entrée et que la sélection n'a rien donné (pas de résultat/tout est signé)
 if [ -z "$req_file" ] ; then
 	echo "aucune requête disponible pour être signée"
 	exit 1
 fi
-# si le CRT asocié existe, on sort car cette vérif a été exécuté lors de la selection des requêtes.
+# si le CRT associé existe, on sort car cette vérif a été exécuté lors de la selection des requêtes.
 # ce test ne sert que pour l'utilsation avec arguments
 if [ -f "${req_file%.csr}.crt" ] ; then
 	echo "Nom de certificat signé déjà utilisé \"${req_file%.csr}.crt\""
@@ -62,17 +62,25 @@ if [ -z "$cfg_file" ] ; then
 fi
 
 # validation e
-#[ -z "$exten" -o $( grep -ec "\s*\[\s*$2\s*\]\s*" ) -lt 1 ] && # choisir extensions dans config
-#si exten toujours vide, quitter sur anomalie extension dans config_file
-
+if [ -z "$exten" ] ; then
+	exten="$( slct $( grep -e "\s*\[.*_ca\s*\]\s*" "$cfg_file" | sed -r 's/\s*\[\s*//g' | sed -r 's/\s*\]\s*//g' | tr '\n' ' ') )_ca"
+fi
+if [ -z "$exten" ] ; then
+	echo "aucune extension disponible pour signer la requête"
+	exit 1
+fi
+if [ $( grep -ec "\s*\[\s*$exten\s*\]\s*" ) -lt 1 ] ; then
+	echo "extension \"$exten\" introuvable dans la configuration \"$cfg_file\""
+	exit 1
+fi
 
 echo openssl ca -config "$cfg_file" -in "$req_file" -out "${req_file%.csr}.crt" -extensions "$exten" -notext
 
 if [ -f "${req_file%.csr}.key" -a -f "${req_file%.csr}.crt" ]; then
-	read -p "Convertir le certificat signé en PKCS#12 ? [y/n] "
-	[ "${REPLY::1}" = "y" ] && openssl pkcs12 -export -inkey "${req_file%.csr}.key" -in "${req_file%.csr}.crt" -out "${req_file%.csr}.p12"
-	read -p "Supprimer les fichiers locaux de cette certification ? [y/n] "
-	[ "${REPLY::1}" = "y" ] && rm -f "${req_file%.csr}."*
+	read -p "Convertir le certificat signé en PKCS#12 ? [y/n] " R
+	[ "${R::1}" = "y" ] && openssl pkcs12 -export -inkey "${req_file%.csr}.key" -in "${req_file%.csr}.crt" -out "${req_file%.csr}.p12"
+	read -p "Supprimer les fichiers locaux de cette certification ? [y/n] " R
+	[ "${R::1}" = "y" ] && rm -f "${req_file%.csr}."*
 fi
 
 echo cp "${req_file%.csr}."* /home/cert/
