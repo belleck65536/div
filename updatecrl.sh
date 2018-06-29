e4b5d7746c432c3c520836c0a8e8f0800b2ada1d
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-29 21:29:01 +0000

    Update updatecrl.sh

--- updatecrl.sh
+++ updatecrl.sh
@@ -12,7 +12,7 @@ let NOW=$( date +%s )+86400*1
 # pour la CRL de chaque CA
 for crl in $(ls -1 "$dir_crl"); do
 	# obtention de la date de fin de validité de la CRL
-	CRL_END=$( openssl crl -in "$crl" -nextupdate -noout 2>/dev/null )
+	CRL_END=$( openssl crl -in "$dir_crl/$crl" -nextupdate -noout 2>/dev/null )
 	CRL_END=$( openssl_time2epoch ${CRL_END#nextUpdate=} )
 
 	# si mise à jour forcée, on modifie l'expiration de la CRL à la nuit des temps
