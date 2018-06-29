62a23408f681342dde1ff0bca4212181dfa27cb2
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-06-29 21:53:06 +0000

    Update requete.sh

--- requete.sh
+++ requete.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-#
-# "${1:-/dev/stdin}" --> prend $1 si stdin est vide
 
 . ./lib.sh
 
-while [ "$NOMFIC" = "" ]; do
-	read -p "Nom de fichier pour la requête (l'extension sera ajoutée automatiquement) : " NOMFIC
-	if [ -f "$WORKDIR$NOMFIC.csr" -o -f "$WORKDIR$NOMFIC.key" ]; then
+while [ "$NOM" = "" ]; do
+	read -p "Nom de fichier pour la requête (l'extension sera ajoutée automatiquement) : " NOM
+	if [ -f "$dir_req/$NOM.csr" -o -f "$dir_key/$NOM.key" ]; then
 		echo "nom de requête déjà utilisé"
-		NOMFIC=""
+		NOM=""
 	fi;
 done
 
@@ -30,7 +28,7 @@ case "$CLEF" in
 esac
 
 echo "Modèle de requête :"
-CFG_FILE=$( slct $( ls ./etc/req*.conf ) )
+CFG_FILE=$( slct $( ls -1 "$dir_cfg/req*.conf" ) )
 
 # recherche des extensions disponibles
 EXT="$( slct $( grep -e "\[.*_ext\s*\]" "$CFG_FILE" | sed -r 's/\[\s*//g' | sed -r 's/_ext\s*\]//g' | tr '\n' ' ') )_ext"
@@ -38,7 +36,7 @@ EXT="$( slct $( grep -e "\[.*_ext\s*\]" "$CFG_FILE" | sed -r 's/\[\s*//g' | sed 
 
 # ajout d'subjectAltName suivant l'extension demandée
 # si mention "san" absente, alors on demande le SAN (logique="without san")
-if [ $(echo "$EXT" | grep -ic "san" ) -eq 0 ] ; then
+if [ $(echo "$EXT" | grep -ic "san") -eq 0 ] ; then
 	printf "%s\n" "Définition du SAN suivant la forme <type>:<valeur>,<type>:<valeur>,..."
 	printf "%s\n" "éléments :\n\tDNS\n\tIP\n\tURI\n\temail\n\tRID\n\tdirName\n\totherName"
 	printf "%s\n" "ex : DNS:www.example.com,IP:0.0.0.0"
@@ -55,6 +53,6 @@ else
 	SANC="#"
 fi
 	
-openssl $KEYARGS >> "$WORKDIR$NOMFIC.key"
-SAN=$SANC openssl req -new -config "$CFG_FILE" -reqexts $EXT -out "$WORKDIR$NOMFIC.csr" -key "$WORKDIR$NOMFIC.key"
-# ./signat.sh $CFG_FILE "$WORKDIR$NOMFIC.csr"
+openssl $KEYARGS >> "$dir_key/$NOM.key"
+SAN=$SANC openssl req -new -config "$CFG_FILE" -reqexts $EXT -out "$dir_req/$NOM.csr" -key "$dir_key/$NOM.key"
+# ./signat.sh $CFG_FILE "$dir_req/$NOM.csr"
