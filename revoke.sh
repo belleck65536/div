038edf273e0e28e5740c56b63dd09d1650fb529e
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-07-05 16:18:08 +0200

    Update revoke.sh

--- revoke.sh
+++ revoke.sh
@@ -8,65 +8,44 @@ else
 fi
 
 
-echo "
-Sélection de l'autorité régissant le certificat à révoquer :
-1. Autorité Racine
-2. Autorité SSL/TLS
-3. Autorité Software
-4. Autorité Email
-"
-while [ "$CA" = "" ]; do
-	read
-	case $REPLY in
-		1) CA=root;;
-		2) CA=tls;;
-		3) CA=software;;
-		4) CA=email;;
-		*) echo "Choix incorrect";;
-	esac
-done
+echo "Sélection de l'autorité régissant le certificat à révoquer : "
+ca=$(slct $(
+	for authority in $( ls -1 "$dir_ca"/ 2>/dev/null ) ; do
+		[ -d "$authority" ] && echo "$(basename "$authority")"
+	done
+))
+[ -z "$ca" ] && exit
 
-echo "
-Numéro de série du certificat ?
-"
-while true; do
-	read SERNUM
-	if [ -f "ca/nc-$CA-ca/$SERNUM.pem" ]; then
-		break;
-	else
-		echo "Numéro de série introuvable sous ca/nc-$CA-ca";
-	fi
-done
 
-echo "
-Raison de la révocation ?
-1. Non spécifié
-2. Certificat compromis
-3. Autorité de certification compromise
-4. Modification de l.affiliation
-5. Certificat remplacé
-6. Cessation d.activité
-"
-while [ "$RAISON" = "" ]
-do
-	read
-	case $REPLY in
-		1) RAISON=unspecified;;
-		2) RAISON=keyCompromise;;
-		3) RAISON=CACompromise;;
-		4) RAISON=affiliationChanged;;
-		5) RAISON=superseded;;
-		6) RAISON=cessationOfOperation;;
-		*) echo "Choix incorrect";;
-	esac
-done
+$tab=$( printf "\t" )
+echo "Numéro de série du certificat ? "
+crt_sn=$( slct $(
+	# obtenir une liste des certificats signés dans le dossier de la CA
+	for crt in $( ls -1 "$dir_ca/$ca"/*.pem ) ; do
+		sn=$( basename "${crt%.pem}" )
+		ligne=$( grep -i -e "[RVE]$tab[0-9]{12}Z$tab.*$tab[0-9A-Fa-f]+$tab.*$tab[^$tab]*" "$dir_ca/$ca/db/$ca.db" )
+		st=$(  echo "$ligne" | cut -f1 )
+		exp=$( echo "$ligne" | cut -f2 )
+		# ne  les cert marqués révoqué/expirés dans la db de la CA
+		[ "$st" = "V" ] && echo "$sn"
+		# ne remonter que les cert encore valides dans la db de la CA
+		[ $exp -ge $NOW ] && echo "$sn"
+	done
+))
+[ -z "$crt_sn" ] && exit
 
-openssl x509 -in "ca/nc-$CA-ca/$SERNUM.pem" -noout -issuer -serial -subject -nameopt RFC2253
+
+echo "Raison de la révocation ? "
+raison=$( slct unspecified keyCompromise CACompromise affiliationChanged superseded cessationOfOperation )
+[ -z "$raison" ] && exit
+
+
+openssl x509 -in "$dir_ca/$ca/$crt_sn.pem" -noout -issuer -serial -subject -nameopt RFC2253
+
 
 read -p "Sûr ? [y/n] "
 if [ "${REPLY::1}" = "y" ]; then
-	openssl ca -config "etc/nc-$CA-ca.conf" -revoke "ca/nc-$CA-ca/$SERNUM.pem" -crl_reason $RAISON;
-	openssl ca -gencrl -config "etc/nc-$CA-ca.conf" -out "crl/nc-$CA-ca.crl" 2>/dev/null
-	# scp -B /mnt/sda1/pki/crl/*.crl 0.0.0.0:/var/www/html/pki/>/dev/null
-	/mnt/sda1/pki/updatecrl.sh
+	openssl ca -config "$dir_etc/$ca.conf" -revoke "$dir_ca/$ca/$crt_sn.pem" -crl_reason $raison;
+#	openssl ca -gencrl -config "etc/nc-$CA-ca.conf" -out "crl/nc-$CA-ca.crl" 2>/dev/null
+#	/mnt/sda1/pki/updatecrl.sh
 fi
