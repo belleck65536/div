4ce4759c91574c8d2464193e5c2be100960b8552
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-07-05 21:04:07 +0000

    Update crl.sh

--- crl.sh
+++ crl.sh
@@ -15,25 +15,23 @@ case "$1" in
 		openssl ca -gencrl -config "$cfg_file" -out "$crl_file" 2>/dev/null
 	;;
 	update)
-		if [ "$2" = "-f" ]; then
-			echo "$( date ) - mise a jour forcée" >>$dir_log/updt.log
-			force_update=1
-		fi
-		for crl in $(ls -1 "$dir_crl"/*); do
+		[ "$2" = "-f" ] && force_update=1 || force_update=0	
+		for crl_file in $(ls -1 "$dir_crl"/*); do
 			if [ "$force_update" = "1" ] ; then
+				echo "$( date ) - mise à jour forcée" >>$dir_log/updt.log
 				# si mise à jour forcée, on modifie l'expiration de la CRL à la nuit des temps
 				CRL_END=0
 			else
 				# obtention de la date de fin de validité de la CRL
-				CRL_END=$( openssl crl -in "$crl" -nextupdate -noout 2>/dev/null )
+				CRL_END=$( openssl crl -in "$crl_file" -nextupdate -noout 2>/dev/null )
 				CRL_END=$( openssl_time2epoch ${CRL_END#nextUpdate=} )
 			fi
 
 			# si la CRL est dépassée, on renouvelle
 			if [ $NOW -ge $CRL_END ]; then
 				echo "$( date ) - renouvellement de $crl" >>updt.log
-				cfg="$dir_cfg/$( basename "${crl%.crl}.conf" )"
-				openssl ca -gencrl -config "$cfg" -out "$crl" 2>/dev/null
+				cfg_file="$dir_cfg/$( basename "${crl_file%.crl}.conf" )"
+				openssl ca -gencrl -config "$cfg_file" -out "$crl_file" 2>/dev/null
 			fi
 		done
 	;;
