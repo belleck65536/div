6c468696619f94d741784b0b1fb8d0e2fba3be31
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-07-06 11:42:11 +0200

    Update revoke.sh

--- revoke.sh
+++ revoke.sh
@@ -22,13 +22,13 @@ crt_sn=$( slct $(
 	# obtenir une liste des certificats signés dans le dossier de la CA
 	for crt in $( ls -1d "$dir_ca/$ca"/*.pem ) ; do
 		sn=$( basename "${crt%.pem}" )
-		ligne=$( grep -i -E "[RVE]$tab[0-9]{12}Z$tab[^$tab]*$tab$sn$tab[^$tab]*$tab[^$tab]*" "$dir_ca/$ca/db/$ca.db" )
-		st=$(  echo "$ligne" | cut -f1 )
-		ex=$( echo "$ligne" | cut -f2 )
+		li=$( grep -i -E "[RVE]$tab[0-9]{12}Z$tab[^$tab]*$tab$sn$tab[^$tab]*$tab[^$tab]*" "$dir_ca/$ca/db/$ca.db" )
+		st=$( echo "$li" | cut -f1 )
+		ex=$( echo "$li" | cut -f2 )
+		# ici on peut avoir un VMETP si date est dans une version qui gère les dates en 32b
 		exp=$( date -d "20${ex:0:2}-${ex:2:2}-${ex:4:2} ${ex:6:2}:${ex:8:2}:${ex:10:2}" +%s )
-		# ne  les cert marqués révoqué/expirés dans la db de la CA
+		# remonter les cert encore valides et NON marqués révoqué/expirés dans la db de la CA
 		[ "$st" = "V" -a "$exp" -ge "$NOW" ] 2>/dev/null && echo "$sn"
-		# ne remonter que les cert encore valides dans la db de la CA
 	done
 ))
 [ -z "$crt_sn" ] && exit
