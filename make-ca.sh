8ab4a7556d8c20ad9d144a9de76382b4c4d7c8b9
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-07-23 18:34:41 +0200

    Update make-ca.sh

--- make-ca.sh
+++ make-ca.sh
@@ -8,58 +8,81 @@ else
 fi
 
 
+# argument en entrée : -i pour le certificat à promouvoir
+while getopts ":i:" opt; do
+	case $opt in
+		i) [ -f "$OPTARG" ] && crt_file="$OPTARG" || die 2 "erreur sur le certificat à promouvoir \"$OPTARG\"" ;;
+		\?) die 3 "Invalid option: -$OPTARG" ;;
+		:)  die 2 "Option -$OPTARG requires an argument." ;;
+	esac
+done
+
+
 # lister les crt dont :
-NOM=$( slct $(
+[ -z "$crt_file" ] && crt_file=$( slct $(
 	for cert in $(ls -1d "$dir_crt"/*.crt) ; do
-		clef="$dir_key/$( basename "${cert%.crt}.key" )"
 		i=0
-		let i+=$( match "$cert" "$clef" )
-		let i+=$( can_sign "$cert" )
-		let i+=$( is_valid "$cert" )
 		# pas déjà été promu signataire
 		[ ! -d "$dir_ca/$( basename "${cert%.crt}" )" ] && let i++
-		[ $i -eq 4 ] && echo "$cert"
+		let i+=$( can_sign "$cert" )
+		let i+=$( is_valid "$cert" )
+		[ $i -eq 3 ] && echo "$cert"
 	done
 ))
-
-[ -z "$NOM" ] && die 1 "aucun certificat sélectionné"
+[ -z "$crt_file" ] && die 4 "aucun certificat sélectionné"
 
 
 # calcul des noms des différents éléments de la future CA
-NOM="$( basename "${NOM%.crt}" )"
+base="$( basename "${crt_file%.crt}" )"
+key_file="$dir_key/$base.key"
+
+[ ! -f mv "$dir_key/$base.key" ] && die 5 "clef inaccessible"
+[ ! -f mv "$dir_crt/$base.crt" ] && die 6 "certificat inaccessible"
+[ ! -f "$dir_crt/$base-chain.pem" ] && die 7 "chaine inaccessible"
+
+
+# vérifier la clef
+[ "$( match "$crt_file" "$key_file" )" != "1" ] && die 5 "Clef privée introuvable/inaccessible/mal nommée"
 
 
 # création de la structure de la nouvelle CA
-mkdir -p "$dir_ca/$NOM/db"
-mkdir -p -m 700 "$dir_ca/$NOM/private"
+mkdir "$dir_ca/$base"
+mkdir "$dir_ca/$base/db"
+mkdir "$dir_ca/$base/private"
 
 
-# déplacer les fichier de l'identifier à promouvoir
-mv "$dir_key/$NOM.key" "$dir_ca/$NOM/private"
-mv "$dir_crt/$NOM.crt" "$dir_ca/$NOM"
+# déplacer les fichier de l'identifier à promouvoir (protection clef et mise en forme CA)
+mv "$dir_key/$base.key" "$dir_ca/$base/private"
+mv "$dir_crt/$base.crt" "$dir_ca/$base"
+mv "$dir_crt/$base-chain.pem" "$dir_ca/$base"
+
+
+# un peu de sécu
+chmod 700 "$dir_ca/$base/private"
+chmod 400 "$dir_ca/$base/private/$base.key"
 
 
 # créer les éléments d'une CA
-cp /dev/null "$dir_ca/$NOM/db/$NOM.db"
-cp /dev/null "$dir_ca/$NOM/db/$NOM.db.attr"
-echo 01 > "$dir_ca/$NOM/db/$NOM.crt.srl"
-echo 01 > "$dir_ca/$NOM/db/$NOM.crl.srl"
+# le cp /dev/null purge la destination si elle existe
+cp /dev/null "$dir_ca/$base/db/$base.db"
+cp /dev/null "$dir_ca/$base/db/$base.db.attr"
+echo 01 >    "$dir_ca/$base/db/$base.crt.srl"
+echo 01 >    "$dir_ca/$base/db/$base.crl.srl"
 
 
 # générer la chaine
 # RFC-5246 7.4.2
 # 1) le cert final, 2) la CA qui a signé [1], 3) la racine qui a signé [2]
 #
-# oulala
-# trouver la CA qui a signé le cert qu'on veut promouvoir
-# ajouter le crt dans le bundle
-# si selfsign, break
-# sinon prendre le cert ajouté et [recursion]
-#cat "$dir_ca/$NOM.crt" "$dir_ca/nc-root-ca.crt" > "$dir_ca/$NOM-ca-chain.pem"
+#cat "$dir_ca/$base.crt" "$dir_ca/nc-root-ca.crt" > "$dir_ca/$base-chain.pem"
+# la chaine sera générée à la [auto]signature du cert
 
 
 # générer une configuration
+# fonction ?
+# demander des paramètres ?
 
 
 # générer une crl
-#openssl ca -gencrl -config etc/$NOM.conf -out $dir_crl/$NOM.crl
+#crl.sh force "$dir_cfg/$base.conf"
+#openssl ca -gencrl -config "$dir_cfg/$base.conf" -out "$dir_crl/$base.crl"
