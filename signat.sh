0373c9551dc1878bbc88f77eb69efad0643dfd5b
belleck65536 <38288102+belleck65536@users.noreply.github.com>
2018-07-24 17:49:13 +0200

    Update signat.sh

--- signat.sh
+++ signat.sh
@@ -24,7 +24,7 @@ done
 	for csr in $( ls -1d "$dir_req"/*.csr 2>/dev/null ) ; do
 		e=0
 		[ -f "$dir_crt/$( basename "${csr%.csr}.crt" )" ] && let e++
-		# tester aussi présence crt promu CA
+		[ -d "$dir_ca/$( basename "${csr%.csr}" )" ] && let i++
 		[ $e -eq 0 ] && echo "$csr"
 	done
 ))
@@ -37,11 +37,13 @@ crt_file="$dir_crt/$( basename "${req_file%.csr}.crt" )"
 # validation c - si pas d'arg, select conf
 [ -z "$cfg_file" ] && cfg_file=$( slct $( ls -1d "$dir_ca"/*.conf 2>/dev/null ) )
 [ -z "$cfg_file" ] && die 4 "aucune configuration disponible pour signer la requête"
+base="${cfg_file%.conf}"
+[ ! -d "$base" ] && die 5 "anomalie lors de l'accès au dossier de la CA demandée"
 
 
 # validation e - si pas d'arg, select extension "*$ext_ca"
 [ -z "$exten" ] && exten="$( slct $( seek_ext "$ext_ca" "$cfg_file" ) )"
-[ -z "$exten" ] && die 5 "aucune extension disponible pour signer la requête"
+[ -z "$exten" ] && die 6 "aucune extension disponible pour signer la requête"
 [ $( grep -c -e "\s*\[\s*$exten\s*\]\s*" "$cfg_file" ) -ne 1 ] && die 5 "anomalie sur extension \"$exten\" dans la configuration \"$cfg_file\""
 
 
